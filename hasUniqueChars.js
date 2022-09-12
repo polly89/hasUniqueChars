13cb7b91ca3c4870ca2a96a3a376bf95a548fb22
Christina Woodhams <cwoodhams89@gmail.com>
2022-09-11 20:21:09 -0400

    Finished problem

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -1,1 +1,15 @@
 // Write your code below
+const randomStr = 'ChristinaRules%#2'
+
+
+const checkSpecialCharacters = (str) => {
+    const format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+    if(format.test(str)){
+      return true;
+    } else {
+      return false;
+    }
+}
+console.log(checkSpecialCharacters(randomStr))
+
